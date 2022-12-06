09c231637d3991cb476c904d1397f7acdb0f012d
Luan Victor Leite da Silva <luan.silva@me.com.br>
2022-12-06 13:49:06 -0300

    Aula 1, 2 e 3

---
+++ Aulas/Curso_cs/Curso_cs/Aula02/Aula02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_cs.Aula02
+{
+    internal class Aula02
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(args.GetValue(0));
+        }
+    }
+}
---
+++ Aulas/Curso_cs/Curso_cs/Aula03/Aula03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_cs.Aula03
+{
+    class Aula03
+    {
+        public static void Main(string[] args)
+        {
+            byte n1 = 10;//0 e 255
+            int num = -10;
+            char letra = '8';
+            float valor = 5.3f;
+            string nome = "Bruno";
+
+            var aux = nome;
+
+            Console.WriteLine(aux);
+        }
+    }
+}
